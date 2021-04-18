e00eefbf792249e7e9cba6cf8cf8d1317dfeef7b
Kyle Kim <kkim@mbp-kkim.fios-router.home>
2021-04-18 17:59:12 -0400

    add first data structure

---
+++ stacks/intro_stacks.py
@@ -0,0 +1,8 @@
+# can be basically a list/array. you essentially need to ensure you follow the LIFO approach with stacks.
+# you can even follow the recursive approach to a stack. 
+class Stack (object):
+
+  __init__ (self):
+    self.items = []
+
+

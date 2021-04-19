2efb7b7fe57d209a1a8cab1ff41bc55fb9e352fa
Kyle Kim <kkim@mbp-kkim.fios-router.home>
2021-04-19 13:25:05 -0400

    set-up

---
+++ binary_trees/binary_trees_intro.py
@@ -0,0 +1,8 @@
+class Node (object):
+  def __init__(self):
+    
+
+class Tree (object):
+  def __init__ (self):
+    self.root = None
+
--- stacks/intro_stacks.py
+++ stacks/intro_stacks.py
@@ -1,8 +1,26 @@
 # can be basically a list/array. you essentially need to ensure you follow the LIFO approach with stacks.
 # you can even follow the recursive approach to a stack. 
+
+# Here instead we will go with the iterative approach with a class that is essentially a wrapper around the list class
 class Stack (object):
 
   __init__ (self):
     self.items = []
 
+  def push (self, item):
+    self.items.append(item)
 
+  def pop (self):
+    self.items.pop()
+
+  def peek(self);
+    return self.items[len(self.items) - 1]
+
+myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+print(myStack.get_stack())
+myStack.push("C")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
